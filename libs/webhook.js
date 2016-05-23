14b7cab25b1b841283f94d15e543823d5ad93729
Jayin Ton <tonjayin@gmail.com>
2016-05-23 12:00:57 +0800

    * 修复对coding分支的处理判断错误 打log  rel #12

--- libs/webhook.js
+++ libs/webhook.js
@@ -7,21 +7,24 @@ var fs = require('fs')
 /**
  * 加载配置
  */
-function load_config(){
+function load_config() {
   // webhook config
   var configs = []
   var files = glob.sync(path.join(__dirname, '../webhooks/**/*.json'))
 
   files.forEach(function (file) {
     var config = JSON.parse(fs.readFileSync(file))
+    //append the file in to config
+    config['file'] = file
     configs.push(config)
+    console.log('load config: ' + file)
   })
-  return configs 
+  return configs
 }
 /**
  * 是否匹配
  */
-function is_match(target, cur){
+function is_match(target, cur) {
   return cur.match(new RegExp(target, 'i')) === null ? false : true;
 }
 
@@ -33,14 +36,20 @@ function is_match(target, cur){
  */
 module.exports.getWebhookConfig = function (platform, https_url, branch) {
   var configs = load_config()
-  for (var index = 0;index < configs.length;index++) {
+  for (var index = 0; index < configs.length; index++) {
     var config = configs[index]
-    
-    if(!is_match(config.platform, platform) || !is_match(config.https_url, https_url)){
-      continue; 
+
+    if (!is_match(config.platform, platform)) {
+      console.log(config.file + ': Unmatch platform');
+      continue;
+    }
+    if (!is_match(config.https_url, https_url)) {
+      console.log(config.file + ': Unmatch https_url');
+      continue;
     }
     //branch 监听处理的分支，若为*则匹配所有分支
-    if ( config.branch !== '*' && config.branch !== branch) {
+    if (config.branch !== '*' && branch.indexOf(config.branch) == -1) {
+      console.log(config.file + ': Unmatch branch');
       continue
     }
     return config
@@ -52,7 +61,7 @@ module.exports.getWebhookConfig = function (platform, https_url, branch) {
  * @param {string} platform the webhook platform
  * @param {object} req http req
  */
-module.exports.parsePlatform = function(platform, req){
+module.exports.parsePlatform = function (platform, req) {
   return Platform[platform].parse(req)
 }
 
